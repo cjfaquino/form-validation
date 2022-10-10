0afda4d2d76dd1e48bdaa4fec2832d9012694ea7
Christian Aquino <cjfaquino@gmail.com>
2022-10-09 22:07:41 -0700

    add email inline errors

--- src/index.js
+++ src/index.js
@@ -16,3 +16,36 @@ const passwordConfirm = form.querySelector("#password-confirm");
 const passwordConfirmError = form.querySelector(
   "#password-confirm + span.error"
 );
+
+const showError = (input, error) => {
+  // email messages
+  if (input.name === "email") {
+    if (input.validity.valueMissing) {
+      error.textContent = "You need to enter an e-mail address.";
+    } else if (input.validity.typeMismatch) {
+      error.textContent = "Entered value needs to be an e-mail address.";
+    } else if (input.validity.tooShort) {
+      error.textContent = `Email should be at least ${input.minLength} characters; you entered ${input.value.length}.`;
+    }
+    error.className = "error active";
+  }
+};
+
+const getSibling = (currentNode) => {
+  const nextSibling = currentNode.nextElementSibling;
+  return nextSibling;
+};
+
+const checkValid = (input) => {
+  const error = getSibling(input);
+  return () => {
+    if (input.validity.valid) {
+      error.textContent = "";
+      error.className = "error";
+    } else showError(input, error);
+  };
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", checkValid(input));
+});
