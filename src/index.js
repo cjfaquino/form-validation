8c8bf87d742086b1e8503b78fa40f2dec12ccab3
Christian Aquino <cjfaquino@gmail.com>
2022-10-09 21:26:43 -0700

    cache DOM elements

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import "./style.css";
 import "./components/countries";
+
+const main = document.querySelector("main");
+const form = main.querySelector("form");
+const inputs = form.querySelectorAll("input");
+const email = form.querySelector("#email");
+const emailError = form.querySelector("#email + span.error");
+const country = form.querySelector("#country");
+const countryError = form.querySelector("#country + span.error");
+const zipCode = form.querySelector("#zip-code");
+const zipCodeError = form.querySelector("#zip-code + span.error");
+const password = form.querySelector("#password");
+const passwordError = form.querySelector("#password + span.error");
+const passwordConfirm = form.querySelector("#password-confirm");
+const passwordConfirmError = form.querySelector(
+  "#password-confirm + span.error"
+);
