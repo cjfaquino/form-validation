fa9d4d70f6c40b8d5030f6b388f24611d6a80e2e
Christian Aquino <cjfaquino@gmail.com>
2022-10-13 21:02:12 -0700

    move to new file

---
+++ src/components/setErrors.js
@@ -0,0 +1,53 @@
+export const setEmailError = (input, error) => {
+  if (input.validity.valueMissing) {
+    error.textContent = "You need to enter an e-mail address.";
+  } else if (input.validity.typeMismatch) {
+    error.textContent = "Entered value needs to be an e-mail address.";
+  } else if (input.validity.tooShort) {
+    error.textContent = `Email should be at least ${input.minLength} characters; you entered ${input.value.length}.`;
+  }
+};
+
+export const setCountryError = (input, error) => {
+  if (input.value === "") {
+    error.textContent = "Please choose a country";
+  }
+};
+
+export const setZipError = (input, error) => {
+  if (input.validity.valueMissing) {
+    error.textContent = "You need to enter a zip code.";
+  } else if (
+    input.validity.typeMismatch ||
+    input.validity.badInput ||
+    input.validity.patternMismatch
+  ) {
+    error.textContent = "Entered value needs to be a 5 digit number.";
+  } else if (input.validity.tooShort || input.validity.rangeUnderflow) {
+    error.textContent = `Zip code should be at least ${input.minLength} characters.`;
+  } else if (input.validity.tooLong || input.validity.rangeOverflow) {
+    error.textContent = `Zip code should be no more than ${input.maxLength} characters.`;
+  }
+};
+
+export const setPwdConfirmError = (input, error) => {
+  const password = document.querySelector("#password");
+  if (input.validity.valueMissing) {
+    error.textContent = "Please re-enter password here.";
+  } else if (input.value !== password.value) {
+    error.textContent = "Passwords do not match.";
+  }
+};
+
+export const setPasswordError = (input, error) => {
+  if (input.validity.valueMissing) {
+    error.textContent = "You need to enter a password.";
+  } else if (
+    input.validity.typeMismatch ||
+    input.validity.badInput ||
+    input.validity.patternMismatch
+  ) {
+    error.textContent =
+      "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.";
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import "./style.css";
 import "./components/countries";
+import {
+  setEmailError,
+  setCountryError,
+  setZipError,
+  setPasswordError,
+  setPwdConfirmError,
+} from "./components/setErrors";
 
 const main = document.querySelector("main");
 const form = main.querySelector("form");
@@ -27,59 +34,6 @@ password.pattern =
 const getSibling = (currentNode) => {
   const nextSibling = currentNode.nextElementSibling;
   return nextSibling;
-};
-
-const setEmailError = (input, error) => {
-  if (input.validity.valueMissing) {
-    error.textContent = "You need to enter an e-mail address.";
-  } else if (input.validity.typeMismatch) {
-    error.textContent = "Entered value needs to be an e-mail address.";
-  } else if (input.validity.tooShort) {
-    error.textContent = `Email should be at least ${input.minLength} characters; you entered ${input.value.length}.`;
-  }
-};
-
-const setPwdConfirmError = (input, error) => {
-  if (input.validity.valueMissing) {
-    error.textContent = "Please re-enter password here.";
-  } else if (input.value !== password.value) {
-    error.textContent = "Passwords do not match.";
-  }
-};
-
-const setPasswordError = (input, error) => {
-  if (input.validity.valueMissing) {
-    error.textContent = "You need to enter a password.";
-  } else if (
-    input.validity.typeMismatch ||
-    input.validity.badInput ||
-    input.validity.patternMismatch
-  ) {
-    error.textContent =
-      "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.";
-  }
-};
-
-const setZipError = (input, error) => {
-  if (input.validity.valueMissing) {
-    error.textContent = "You need to enter a zip code.";
-  } else if (
-    input.validity.typeMismatch ||
-    input.validity.badInput ||
-    input.validity.patternMismatch
-  ) {
-    error.textContent = "Entered value needs to be a 5 digit number.";
-  } else if (input.validity.tooShort || input.validity.rangeUnderflow) {
-    error.textContent = `Zip code should be at least ${input.minLength} characters.`;
-  } else if (input.validity.tooLong || input.validity.rangeOverflow) {
-    error.textContent = `Zip code should be no more than ${input.maxLength} characters.`;
-  }
-};
-
-const setCountryError = (input, error) => {
-  if (input.value === "") {
-    error.textContent = "Please choose a country";
-  }
 };
 
 const showError = (input) => {
